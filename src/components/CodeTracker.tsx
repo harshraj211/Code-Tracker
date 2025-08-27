31266751aa8ea869068e4f1fe1127eee52a6ae08
Harsh Raj <harshraj84068@gmail.com>
2025-08-27 06:18:29 +0000

    i said all the subject not only which i named to uou like dsa linux cybe

--- src/components/CodeTracker.tsx
+++ src/components/CodeTracker.tsx
@@ -18,6 +18,9 @@ const initialSubjects: Subject[] = [
       { id: 'js-dom', name: 'DOM Manipulation' },
       { id: 'js-es6', name: 'ES6+ Features' },
       { id: 'js-async', name: 'Async/Await' },
+      { id: 'js-objects', name: 'Objects and Prototypes' },
+      { id: 'js-arrays', name: 'Array Methods' },
+      { id: 'js-events', name: 'Event Handling' },
     ],
   },
   {
@@ -28,7 +31,9 @@ const initialSubjects: Subject[] = [
       { id: 'css-flexbox', name: 'Flexbox' },
       { id: 'css-grid', name: 'Grid' },
       { id: 'css-responsive', name: 'Responsive Design' },
-      { id: 'css-animations', name: 'Animations' },
+      { id: 'css-animations', name: 'Animations & Transitions' },
+      { id: 'css-vars', name: 'Custom Properties' },
+      { id: 'css-pre', name: 'Pre-processors (Sass/Less)' },
     ],
   },
   {
@@ -40,6 +45,8 @@ const initialSubjects: Subject[] = [
       { id: 'react-hooks', name: 'Hooks' },
       { id: 'react-context', name: 'Context API' },
       { id: 'react-router', name: 'React Router' },
+      { id: 'react-performance', name: 'Performance Optimization' },
+      { id: 'react-testing', name: 'Testing with Jest & RTL' },
     ],
   },
   {
@@ -51,6 +58,78 @@ const initialSubjects: Subject[] = [
         {id: 'tailwind-theme', name: 'Customizing Theme'},
         {id: 'tailwind-plugins', name: 'Using Plugins'},
         {id: 'tailwind-jit', name: 'JIT Compilation'},
+    ]
+  },
+  {
+    id: 'dsa',
+    name: 'Data Structures & Algos',
+    topics: [
+      {id: 'dsa-arrays', name: 'Arrays & Strings'},
+      {id: 'dsa-linked-lists', name: 'Linked Lists'},
+      {id: 'dsa-stacks-queues', name: 'Stacks & Queues'},
+      {id: 'dsa-trees', name: 'Trees (Binary, BST)'},
+      {id: 'dsa-graphs', name: 'Graphs'},
+      {id: 'dsa-heaps', name: 'Heaps & Priority Queues'},
+      {id: 'dsa-sorting', name: 'Sorting Algorithms'},
+      {id: 'dsa-searching', name: 'Searching Algorithms'},
+      {id: 'dsa-dp', name: 'Dynamic Programming'},
+    ]
+  },
+  {
+    id: 'c-plus-plus',
+    name: 'C++',
+    topics: [
+      {id: 'cpp-basics', name: 'Basics & Syntax'},
+      {id: 'cpp-pointers', name: 'Pointers & Memory'},
+      {id: 'cpp-oop', name: 'Object-Oriented Programming'},
+      {id: 'cpp-stl', name: 'Standard Template Library (STL)'},
+      {id: 'cpp-concurrency', name: 'Concurrency'},
+    ]
+  },
+  {
+    id: 'python',
+    name: 'Python',
+    topics: [
+      {id: 'python-basics', name: 'Data Types & Variables'},
+      {id: 'python-ds', name: 'Data Structures (Lists, Dicts)'},
+      {id: 'python-functions', name: 'Functions & Lambdas'},
+      {id: 'python-oop', name: 'Classes & Objects'},
+      {id: 'python-modules', name: 'Modules & Packages'},
+      {id: 'python-django', name: 'Django Framework'},
+      {id: 'python-flask', name: 'Flask Framework'},
+    ]
+  },
+  {
+    id: 'java',
+    name: 'Java',
+    topics: [
+      {id: 'java-basics', name: 'Core Concepts & JVM'},
+      {id: 'java-oop', name: 'Object-Oriented Principles'},
+      {id: 'java-collections', name: 'Collections Framework'},
+      {id: 'java-concurrency', name: 'Multithreading'},
+      {id: 'java-spring', name: 'Spring Framework'},
+    ]
+  },
+  {
+    id: 'linux',
+    name: 'Linux',
+    topics: [
+      {id: 'linux-cli', name: 'Command Line Interface'},
+      {id: 'linux-fs', name: 'File System & Permissions'},
+      {id: 'linux-shell', name: 'Shell Scripting'},
+      {id: 'linux-process', name: 'Process Management'},
+      {id: 'linux-networking', name: 'Networking Tools'},
+    ]
+  },
+  {
+    id: 'cybersecurity',
+    name: 'Cybersecurity',
+    topics: [
+      {id: 'cyber-netsec', name: 'Network Security'},
+      {id: 'cyber-crypto', name: 'Cryptography'},
+      {id: 'cyber-websec', name: 'Web App Security (OWASP)'},
+      {id: 'cyber-pentest', name: 'Penetration Testing'},
+      {id: 'cyber-forensics', name: 'Digital Forensics'},
     ]
   }
 ];
