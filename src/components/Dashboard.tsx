be08719b236e8810b77218d4c2ca22ed9c269f59
Harsh Raj <harshraj84068@gmail.com>
2025-08-27 06:19:59 +0000

    calendar lil bit on right  like the right side of the screen

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -68,7 +68,7 @@ export function Dashboard({
               />
         </div>
 
-        <div className="space-y-6">
+        <div className="space-y-6 lg:col-start-3">
           <Card>
             <CardHeader>
               <CardTitle>Calendar</CardTitle>
